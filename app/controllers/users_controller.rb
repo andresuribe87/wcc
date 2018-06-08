301771610b04060e70707a48b34c2e7ad280cf92
Andres Uribe <andresuribe87@gmail.com>
2018-06-08 21:21:18 +0000

    Added admin capabilities to update admin things

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :admin_user, only: [:create, :destroy]  
-  before_action :correct_user,   only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update], unless: -> { !current_user.nil? and current_user.is_admin? }
 
 
   # GET /users
@@ -109,8 +109,8 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, flash: { success: 'User was successfully updated.' } }
+      if (!user_params[:is_admin] or current_user.is_admin) and @user.update(user_params)
+        format.html { redirect_to action: 'edit', flash: { success: 'User was successfully updated.' } }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -141,7 +141,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+      params.require(:user).permit(:email, :name, :password, :password_confirmation, :is_admin)
     end
 
     def correct_user
